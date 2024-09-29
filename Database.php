055ce2f62c797df12df856f21855f25df7096b54
Muhammedh Shadir <shadir@fatbeehive.com>
2024-09-29 23:08:44 +0530

    Database class and connection

---
+++ Database.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+    public $conn;
+
+    /**
+     * Construct for database class
+     * 
+     * @param array $config
+     */
+    public function __construct($config)
+    {
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+
+        try {
+            $this->conn = new PDO($dsn, $config['username'], $config['password']);
+        } catch (PDOException $e) {
+            throw new Exception("Database connection failed: {$e->getMessage()}");
+        }
+    }
+}

a99c742c00b1d1023a30180612e9788df0d3f803
Muhammedh Shadir <shadir@fatbeehive.com>
2024-09-29 20:20:51 +0530

    Router refactor to class

---
+++ Router.php
@@ -0,0 +1,80 @@
+<?php
+
+class Router {
+    protected $routes = [];
+
+    public function registerRoute($method, $uri, $controller) {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller
+        ];
+    }
+
+    /**
+     * Load error page
+     *
+     * @param integer $httpCode
+     * @return void
+     */
+    public function error($httpCode = 404) {
+        http_response_code($httpCode);
+        require loadView("error/{$httpCode}");
+        exit;
+    }
+
+    /**
+     * Add a get route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function get($uri, $controller) {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+        /**
+     * Add a post route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function post($uri, $controller) {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+            /**
+     * Add a put route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function put($uri, $controller) {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+            /**
+     * Add a delete route
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function delete($uri, $controller) {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    public function route($uri, $method) {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+
+        $this->error();
+    }
+}

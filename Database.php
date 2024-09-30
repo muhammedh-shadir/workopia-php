2d089200fae51eb9f4ad7d51fedcc042f4680ab7
Muhammedh Shadir <shadir@fatbeehive.com>
2024-09-30 12:19:23 +0530

    Query method and fetch listings

--- Database.php
+++ Database.php
@@ -18,9 +18,26 @@ class Database {
         ];
 
         try {
-            $this->conn = new PDO($dsn, $config['username'], $config['password']);
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
         } catch (PDOException $e) {
             throw new Exception("Database connection failed: {$e->getMessage()}");
+        }
+    }
+
+    /**
+     * Query the database
+     * 
+     * @param string $query
+     * @return PDOStatment
+     * @throws PDOException
+    */
+    public function query($query) {
+        try {
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        } catch (PDOException $e) {
+            throw new Exception("Query failed to execute: {$e->getMessage()} ");
         }
     }
 }
